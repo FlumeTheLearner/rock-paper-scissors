d462f797af845453852be1c30ada3236b2150065
Jon Hii <jonathan.hii@outlook.com>
2022-03-09 22:28:45 +1000

    removed bulk draw results, added ! to strings.

--- main.js
+++ main.js
@@ -10,29 +10,21 @@ function computerPlay() {
 // function returns the expected value - tested in browser
 
 function playRound(playerSelection, computerSelection) {
-  // draw result
-  if (playerSelection == "rock" && computerSelection == "rock") {
-    result = "Draw";
-  } else if (playerSelection == "paper" && computerSelection == "paper") {
-    result = "Draw";
-  } else if (playerSelection == "scissors" && computerSelection == "scissors") {
-    result = "Draw";
-    // win result
-  } else if (playerSelection == "rock" && computerSelection == "scissors") {
-    result = "You win! Rock beats Scissors";
+  if (playerSelection == "rock" && computerSelection == "scissors") {
+    result = "You win! Rock beats Scissors!";
   } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    result = "You win! Rock beats Scissors";
+    result = "You win! Rock beats Scissors!";
   } else if (playerSelection == "paper" && computerSelection == "rock") {
-    result = "You win! Rock beats Scissors";
+    result = "You win! Rock beats Scissors!";
     // lose result
   } else if (computerSelection == "rock" && playerSelection == "scissors") {
-    result = "You lose! Rock beats Scissors";
-  } else if (computerSelection == "scissors" && playerSelection == "paper") {
-    result = "You lose! Rock beats Scissors";
+    result = "You lose! Rock beats Scissors!";
+  } else if (computerSelection == "scissors!" && playerSelection == "paper") {
+    result = "You lose! Rock beats Scissors!";
   } else if (computerSelection == "paper" && playerSelection == "rock") {
-    result = "You lose! Rock beats Scissors";
+    result = "You lose! Rock beats Scissors!";
   } else {
-    result = "Error has occured";
+    result = "DRAW! Try again!";
   }
   console.log(result);
 }
